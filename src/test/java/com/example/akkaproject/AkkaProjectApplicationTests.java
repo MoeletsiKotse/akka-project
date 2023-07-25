b32d7b1cf164d213e3134724e3e57b42b15c02c2
moeletsikotse <moeletsimoeletsi4@gmail.com>
2023-07-25 20:28:20 +0200

    initial commit

---
+++ src/test/java/com/example/akkaproject/AkkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
